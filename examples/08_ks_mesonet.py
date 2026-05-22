75c2d8a5071d238550f03f5e11b4705991ff74e0
Andres Patrignani <andrespatrignani@ksu.edu>
2026-05-21 23:32:12 -0500

    Added annotated code to docs

--- examples/08_ks_mesonet.py
+++ examples/08_ks_mesonet.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 def get_ks_mesonet(station, start_date, end_date, variables, interval='day'):
     """Function that requests weather data from the Kansas Mesonet."""
     fmt = '%Y%m%d%H%M%S'
+    print("3",start_date, type(start_date))
+
     start_date = pd.to_datetime(start_date).strftime(fmt)
     end_date = pd.to_datetime(end_date).strftime(fmt)
     variables = ','.join(variables)
@@ -19,27 +21,36 @@ def get_ks_mesonet(station, start_date, end_date, variables, interval='day'):
     df = pd.read_csv(url, na_values='M')
     return df
 
-# guile state variables
+
+# State
 data = gui.state(None)
 station = gui.state('Manhattan')
-start_date = 
-end_date =
-interval =
+start_date = gui.state('2026-01-01')
+end_date = gui.state('2026-05-01')
+variables = gui.state(['TEMP2MAVG'])
+interval = gui.state('day')
 
+print("1",start_date)
+
+# Callback
 def run():
-    data.set(get_ks_mesonet(station,start_date,end_date,variables,interval))
+    df = get_ks_mesonet(station.value,start_date.value,end_date.value,variables.value,interval.value)
+    data.set(df)
 
-# guile layout
+
+# Layout
 @gui.app("Kansas Mesonet Data Explorer", width=800, height=400)
 def ui():
     with gui.row():
         with gui.col():
-            station = gui.select(label='Station', options=['Manhattan','Ashland Bottoms'])
-            start_date = gui.date_input("From", key="from")
-            end_date = gui.date_input("To", key="to")
-            variables = gui.multiselect(["TEMP2MAVG","SRAVG"])
-            interval = gui.select(label='Interval', options=[('day','Daily'),('hourly','Hourly')])
+            gui.select(label='Station', options=['Manhattan','Ashland Bottoms'], on_change=station.set, key='station')
+            gui.date_input("Start date", on_change=start_date.set, key="from")
+            gui.date_input("End date", on_change=end_date.set, key="to")
+            gui.multiselect(["TEMP2MAVG","SRAVG"], on_change=variables.set, key='variables')
+            gui.select(label='Interval', options=[('day','Daily'),('hourly','Hourly')], on_change=interval.set, key='interval')
             gui.button("Request", on_click=run)
+            print("2",start_date)
+
 
         with gui.col():
             if data.value is None:
